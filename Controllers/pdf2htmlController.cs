c9f314b6c08c7abcedd42dc9183fde6fbbb32b8f
Thach Mai <contact@thachmai.com>
2014-11-30 17:29:47 +0100

    First commit

---
+++ Controllers/pdf2htmlController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMvc.Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class pdf2htmlController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var body = Request.Body;
+            var fileName = String.Format("Convert_{0}", Guid.NewGuid());
+
+            using (FileStream fs = System.IO.File.Create(fileName + ".pdf"))
+            {
+                await body.CopyToAsync(fs);
+            }
+
+            var p = new Process();
+            p.StartInfo.FileName = "pdf2htmlEX";
+            p.StartInfo.Arguments = fileName + ".pdf";
+            p.Start();
+            p.WaitForExit();
+
+            var buffer = System.IO.File.ReadAllBytes(fileName + ".html");
+
+            return new FileContentResult(buffer, "text/html");
+        }
+    }
+}
